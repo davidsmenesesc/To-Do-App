1678853640b2bae39213627bb06d3fb0a2eaf014
David Meneses <David.Meneses@endava.com>
2022-08-17 13:34:17 -0500

    message

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -106,7 +106,7 @@ function AddItemForm({ onNewItem }) {
                         disabled={!newItem.length}
                         className={submitting ? 'disabled' : ''}
                     >
-                        {submitting ? 'Adding...' : 'Add new item'}
+                        {submitting ? 'Adding...' : 'Add new'}
                     </Button>
                 </InputGroup.Append>
             </InputGroup>
